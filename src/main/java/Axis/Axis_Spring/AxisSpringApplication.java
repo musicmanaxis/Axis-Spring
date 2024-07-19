9ca1127f0c50cf4af338e10c54010a8d4c4ae375
Axis <erlia@naver.com>
2024-07-19 23:01:06 +0900

    1

---
+++ src/main/java/Axis/Axis_Spring/AxisSpringApplication.java
@@ -0,0 +1,13 @@
+package Axis.Axis_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxisSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Axis/Axis_Spring/AxisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Axis.Axis_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
