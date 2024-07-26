bacec456e892f4ce9ea8ad0d4aefd0fbd41edd8b
Erlia <erlia@naver.com>
2024-07-26 11:10:30 +0900

    1

--- src/main/java/Axis/Axis_Spring/controller/PostController.java
+++ src/main/java/Axis/Axis_Spring/controller/PostController.java
@@ -16,21 +16,24 @@ public class PostController {
     public String postMethod(){
         return "Post Hello World";
     }
+
     // http://localhost:8080/api/v1/post-api/default
+/*
+ 아래는 json형태로 바디값에 덧붙여서 전송해봐서 아래 2개의 메서드를 실행하는 방식이다.
 
+        {
+          "name":"erlia",
+          "email":"erlia@naver.com",
+           "group":"axis",
+          "sex":"man"
+        }
+   */
 
-//  아래는 json형태로 바디값에 덧붙여서 전송해봐서 아래 2개의 메서드를 실행하는 방식이다.
-//    {
-//            "name":"erlia",
-//            "email":"erlia@naver.com",
-//            "group":"axis",
-//            "sex":"man"
-//    }
-
-
-
-    //@RequestBody 어노테이션은 HTTP 요청의 본문(body)에 포함된 JSON, XML 또는 기타 포맷의 데이터를 Java 객체로 변환하는 역할을 합니다.
-    // 이 어노테이션을 사용하면 클라이언트가 요청 본문에 포함한 데이터를 쉽게 접근할 수 있습니다.
+/*
+  @RequestBody 어노테이션은 HTTP 요청의 본문(body)에 포함된 JSON, XML 또는
+  기타 포맷의 데이터를 Java 객체로 변환하는 역할을 합니다.
+  이 어노테이션을 사용하면 클라이언트가 요청 본문에 포함한 데이터를 쉽게 접근할 수 있습니다.
+*/
 
 
     @PostMapping(value = "/member")
