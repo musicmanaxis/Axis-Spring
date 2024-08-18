535868cfb3c185dcd1a5ee915636867b549bfbe6
Axis <erlia@naver.com>
2024-08-19 05:49:31 +0900

    1 2 3 4

--- src/main/java/Axis/Axis_Spring/controller/HelloController.java
+++ src/main/java/Axis/Axis_Spring/controller/HelloController.java
@@ -2,7 +2,7 @@ package Axis.Axis_Spring.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController   //Controller라는 것을 알려주는 역할
 public class HelloController {
     //@RequestMapping(value = "/hello", method = RequestMethod.GET)   ->예전방식
     @GetMapping("/hello")
