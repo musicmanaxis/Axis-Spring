bf0dc5b07df078ea78b771c2f282dd4da3b11023
Erlia <erlia@naver.com>
2024-08-19 18:07:10 +0900

    1

--- src/main/java/Axis/Axis_Spring/controller/GetController.java
+++ src/main/java/Axis/Axis_Spring/controller/GetController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+//@GetMapping 사용방법...
+
 @RestController //Rest한 컨트롤러임을 선언
-@RequestMapping("api/v1/get-api")   //공통되는 url을 사용하기 위해서 사용,  밑에 각 @GetMapping의 상위페이지
+@RequestMapping("api/v1/get-api")  //버전관리를 위해 V1이라고 표시...
+//공통되는 url을 사용하기 위해서 사용,  밑에 각 @GetMapping의 상위페이지
+
 public class GetController {
 
-    @GetMapping(value="variable1/{variable}")
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "Erlia";
+    }
+    //http://localhost:8080/api/v1/get-api/name
+
+    @GetMapping(value="/variable1/{variable}")
     public String getVaiable1(@PathVariable String variable){
         return variable;
     }
@@ -28,7 +38,7 @@ public class GetController {
     // http://localhost:8080/api/v1/get-api/variable1/{String 값}
 
     @GetMapping(value = "request1")
-    public String getRequestParam(@RequestParam String name,
+    public String getRequestParam(@RequestParam String name,   //각각의 들어오는 값을 이 객체들로 받는다.
                                   @RequestParam String email,
                                   @RequestParam String group){
         return "이름:"+name+", 이메일:"+email+", 조직:"+group;
@@ -36,6 +46,7 @@ public class GetController {
     //@RequestParam :Get 형식의 요청에서 쿼리 문자열을 전달하기 위해 사용하는 방법
     //'?'를 기준으로 우측에 {키}={값}의 형태로 전달되며, 복수 형태로 전달할 경우 &를 사용함
     // http://localhost:8080/api/v1/get-api/request1?name=erlia&email=erlia@naver.com&group=axis 웹페지에 입력확인
+    //위에 값들을 @RequestParam String name 같은 객체에 넘겨준다.
 
 
     @GetMapping(value = "request2")  //이방법은 입력것이 url에 무작위로 들어올때 쓰는 방법
@@ -46,8 +57,10 @@ public class GetController {
         param.entrySet().forEach(map -> {sb.append(map.getKey() + ":" + map.getValue() + "\n"); });
         return sb.toString();   //스트링빌더를 스트링으로 변환해서 출력
     }
-    //위 예시 코드는 어떤 요청값이 들어올지 모를 경우 사용하는 방식
-    // http://localhost:8080/api/v1/get-api/request2?name=마&email=엑시스@naver.com&group=하하하&sex=남자
+    //위 예시 코드는 어떤 요청값이 뭐가 들어올지 모를 경우 사용하는 방식
+    //1. http://localhost:8080/api/v1/get-api/request2?name=마&email=엑시스@naver.com&group=하하하&sex=남자
+    //2. http://localhost:8080/api/v1/get-api/request2?성별=골라보자
+    //1.2번과 같이 아무렇게나 키=값 형태로 갯수와 상관없이 넘겨주면 그대로 출력된다.
 
 
     //키와 값이 정해져 있지만 받아야할 파라미터가 많을 경우 DTO 객체를 이용하는 방식
