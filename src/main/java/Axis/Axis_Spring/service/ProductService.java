929b3a0f68a0d147a31fec83d36786a754906b5f
Erlia <erlia@naver.com>
2024-08-27 15:38:32 +0900

    1

---
+++ src/main/java/Axis/Axis_Spring/service/ProductService.java
@@ -0,0 +1,11 @@
+package Axis.Axis_Spring.service;
+
+import Axis.Axis_Spring.data.dto.ProductDto;
+
+
+//loose coupling DB가 바뀔수 있는 경우를 상정해서 우선 인터페이스를 작성한다.
+public interface ProductService {
+ ProductDto saveProduct(String productId, String productName, int productPrice, int productStock);
+ ProductDto getProduct(String productId);
+
+}
