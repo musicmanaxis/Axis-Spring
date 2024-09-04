6b0805e861ab03c590eac91d4adb2701bd792a4b
Erlia <erlia@naver.com>
2024-09-04 19:09:12 +0900

    1

--- src/main/java/Axis/Axis_Spring/data/repository/ProductRepository.java
+++ src/main/java/Axis/Axis_Spring/data/repository/ProductRepository.java
@@ -7,5 +7,5 @@ public interface  ProductRepository extends JpaRepository<ProductEntity, String>
 
 }
 
-//JpaRepository<레포지토리가 사용할 엔티티, id값의 데이터 타입>
+//JpaRepository<레포지토리가 사용할 엔티티, primary key의 id값 데이터 타입>
 //JpaRepository를 사용하게 되면 @Repository을 사용할 필요가 없다, 기본적인 CRUD메서드를 제공한다.
