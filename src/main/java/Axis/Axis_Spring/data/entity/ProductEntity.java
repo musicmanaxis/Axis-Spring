85c244ab597ad06155dd6ef567142ee9e089ccec
Erlia <erlia@naver.com>
2024-09-04 16:50:10 +0900

    1

--- src/main/java/Axis/Axis_Spring/data/entity/ProductEntity.java
+++ src/main/java/Axis/Axis_Spring/data/entity/ProductEntity.java
@@ -15,8 +15,8 @@ import  Axis.Axis_Spring.data.dto.ProductDto;
 @AllArgsConstructor
 @Table(name="product")  //ProductEntity를 기반으로 디비에 테이블을 자동으로 생성해주는 옵션
 // 이때 테이블의 이름을 name="product"으로 지정
-public class ProductEntity {
 
+public class ProductEntity {
 
     @Id //DB의 프라이머리 키와 동일한 의미이고 productId에 속성을 부여하였다.
     String productId;
